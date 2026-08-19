265881693eebb6e0f2cb618aa891cbb7b8f095b9
AmanuCodes <zescholarboy@gmail.com>
2026-08-20 00:13:21 +0300

    Update header in index.html

---
+++ 2nd/assets/js/script.js
@@ -0,0 +1,18 @@
+function showmenu() {
+  const menu1 = document.getElementById('menu1');
+  const menuhid = document.querySelectorAll('.menuhid')
+  if (menu1.style.display === 'block') {
+    menu1.style.display = 'none';
+    menuhid.forEach((e)=>
+    {
+      e.style.visibility='visible'
+    });
+  } 
+  else {
+    menu1.style.display = 'block';
+    menuhid.forEach((e)=>
+    {
+      e.style.visibility='hiden'
+    });
+  }
+}
