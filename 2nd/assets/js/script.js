7b994c90eb7ed8c8e93d38bd732fa6ae744c9a5f
AmanuCodes <zescholarboy@gmail.com>
2026-08-20 13:31:56 +0300

    Update hscrolled and body, sections

--- 2nd/assets/js/script.js
+++ 2nd/assets/js/script.js
@@ -1,19 +1,17 @@
 function showmenu() {
   const menu1 = document.getElementById('menu1');
-  const menuhid = document.querySelectorAll('.menuhid')
+  const menuhid = document.querySelectorAll('.menuhid');
+
   if (menu1.style.display === 'block') {
     menu1.style.display = 'none';
-    menuhid.forEach((e)=>
-    {
-      e.style.visibility='visible'
+    menuhid.forEach((e) => {
+      e.style.visibility = 'visible';
     });
-  } 
-  else {
+  } else {
     menu1.style.display = 'block';
-    menuhid.forEach((e)=>
-    {
-      e.style.visibility='hidden'
+    menuhid.forEach((e) => {
+      e.style.visibility = 'hidden';
     });
-    menu1.className="borred"
+    menu1.className = "borred";
   }
 }
